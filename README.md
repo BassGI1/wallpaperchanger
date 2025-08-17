99a8f3d0d229387601150300c27ac3fe6f8b3bed
Bassam <basmaym148@gmail.com>
2025-08-17 02:28:21 -0400

    updated

--- README.md
+++
@@ -1,1 +0,0 @@
-Just a small app that I made using python. It changes your wallpaper based on an input query given by the user.
--- wallpaper.py
+++ wallpaper.py
@@ -1,22 +1,22 @@
 from requests import get
-from json import loads
-from random import randint
-from os import getcwd, path
 from ctypes import windll
+from os import path, getcwd
+from playwright.sync_api import sync_playwright
 
-headers = {"Authorization": "563492ad6f91700001000001e6da963077b249b7a8937d73c85ddf78"}
-q = input("What type of background would you like?\n")
-params = {"query": q, "per_page": 80, "orientation": "landscape", "page": 1}
+URI = "https://apod.nasa.gov/apod/astropix.html"
+JAVASCRIPT = """() => document.getElementsByTagName(\"IMG\")[0].src"""
+
+def get_image_uri():
+    with sync_playwright() as pl:
+        browser = pl.chromium.launch()
+        page = browser.new_page()
+        page.goto(URI)
+        return page.evaluate(JAVASCRIPT)
+
+
+image = get(get_image_uri()).content
 imagePath = path.join(getcwd(), "wallpaper.png")
+with open("wallpaper.png", "wb") as file:
+    file.write(image)
 
-try:
-    total = loads(get('https://api.pexels.com/v1/search', headers=headers, params=params).text)['total_results']
-    params["page"] = randint(1, int(total / 80)) if total > 80 else 1
-    image = loads(get('https://api.pexels.com/v1/search', headers=headers, params=params).text)['photos']
-    image = get(image[randint(0, len(image) - 1)]['src']['large2x'], headers=headers).content
-    with open("wallpaper.png", 'wb') as handler:
-        handler.write(image)
-    windll.user32.SystemParametersInfoW(20, 0, imagePath, 0)
-
-except:
-    print("Invalid search. Please try again with a valid query.")
+windll.user32.SystemParametersInfoW(20, 0, imagePath, 0)
